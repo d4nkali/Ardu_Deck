c74d41505c4cc7d059a0a8325e14204a93ac50b2
d4nkali <133923419+d4nkali@users.noreply.github.com>
2023-12-21 15:45:06 -0300

    Definindo os pinos

--- Ardu_Deck_Leonardo/Ardu_Deck_Leonardo.ino
+++ Ardu_Deck_Leonardo/Ardu_Deck_Leonardo.ino
@@ -7,12 +7,30 @@
 
 // Inclusão das bibliotecas
 
-  #include <Keyboard.h>
   #include <HID-Project.h>
 
+// Definindo os pinos
+
+  #define led LED_BUILTIN // Define o pino digital 13 como led
+
+  #define botao_1 12 // Define o pino digital 12 como botão 1 do gerenciador de tarefas
+  #define botao_2 11 // Define o pino digital 11 como botão 2 do cmd
+  #define botao_3 10 // Define o pino digital 10 como botão 3 do explorador de arquivos
+  #define botao_4 9 // Define o pino digital 9 como botão 4 de encerrar programas 
+  #define botao_5 8 // Define o pino digital 8 como botão 5 do bloco de notas 
+  #define botao_6 7 // Define o pino digital 7 como botao 6 das configuraçoes rapidas
+  #define botao_7 6 // Define o pino digital 6 como botao 7 das configuraçoes do PC
+  #define botao_8 5 // Define o pino digital 5 como botao 8 as notificações do PC
+
+  #define botao_mute 4 // Define o pino digital 4 como botao de mutar o volume do PC
+  #define botao_menos 3 // Define o pino digital 3 como botao de diminuir o volume do PC
+  #define botao_mais 2 // Define o pino digital 2 como botao de aumentar o volume do PC
+
+  #define botao_voltar A0 // Define o pino analogico A0 como botao de voltar a midia do PC
+  #define botao_pause_rep A1  // Define o pino analogico A0 como botao de pausar ou reproduzir a midia do PC
+  #define botao_avancar A2  // Define o pino analogico A0 como botao de avançar a midia do PC
+
 void setup() {
-
-
 
 
 }
