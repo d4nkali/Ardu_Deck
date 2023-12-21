d36cbf64db75f80697a1912c0af418e577921e04
d4nkali <133923419+d4nkali@users.noreply.github.com>
2023-12-21 13:43:17 -0300

    Adicionando o arquivo do programa com a inclusão das bibliotecas

---
+++ Ardu_Deck_Leonardo/Ardu_Deck_Leonardo.ino
@@ -0,0 +1,26 @@
+/*
+
+  Programa do Ardu Deck com Arduino Leornado
+  Autor: d4nkali 21/12/2023 v1.0
+
+*/
+
+// Inclusão das bibliotecas
+
+  #include <Keyboard.h>
+  #include <HID-Project.h>
+
+void setup() {
+
+
+
+
+}
+
+void loop() {
+
+
+
+}
+
+// FIM!
