4ba827ba00a0e49d437483c76b8bda9cffbe8c9b
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-01-04 16:09:33 -0300

    Adicionando atalhos dos botões 4 e 5

--- Ardu_Deck_Pro_Micro/Ardu_Deck_Pro_Micro.ino
+++ Ardu_Deck_Pro_Micro/Ardu_Deck_Pro_Micro.ino
@@ -128,6 +128,42 @@ void loop() {
     
     }
 
+    if (digitalRead(botao_4) == LOW) {  // Se o botão estiver pressionado, então:
+  
+      delay(150);  // Aguarda um curto período para evitar debounce
+      digitalWrite(led, HIGH); // Liga o led
+    
+      Keyboard.press(KEY_LEFT_ALT);  // Pressiona a tecla Alt
+      Keyboard.press(KEY_F4);  // Pressiona a tecla F4
+      Keyboard.releaseAll();  // Solta todas as teclas
+    
+      delay(3000);  // Aguarda um tempo para a janela fechar
+      digitalWrite(led, LOW); // Desliga o led    
+    
+    }
+
+    // Abrir Notepad
+
+    if (digitalRead(botao_5) == LOW) {  // Se o botão estiver pressionado, então:
+  
+      delay(150);  //Aguarda um curto período para evitar debounce
+      digitalWrite(led, HIGH); // Liga o led
+    
+      Keyboard.press(KEY_LEFT_GUI);  // Pressiona a tecla Windows/Iniciar/Super
+      Keyboard.press('r');  // Pressiona a tecla R
+      Keyboard.releaseAll();  // Solta todas as teclas
+    
+      delay(2000);  // Aguarda um tempo para a janela Executar aparecer
+    
+      Keyboard.print("notepad");  // Digita "cmd" na janela Executar    
+      Keyboard.press(KEY_RETURN);  // Pressiona a tecla Enter para executar o comando
+      Keyboard.releaseAll();  // Solta todas as teclas
+        
+      delay(5000);  // Aguarda um tempo para o prompt de comando aparecer
+      digitalWrite(led, LOW); // Desliga o led
+    
+    }
+
 }
 
 // FIM!
