3247c3a12c8b6ae4a16370d69ab728bfc711a444
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-01-07 11:31:43 -0300

    Adicionando atalhos dos botões 6, 7 e 8

--- Ardu_Deck_Pro_Micro/Ardu_Deck_Pro_Micro.ino
+++ Ardu_Deck_Pro_Micro/Ardu_Deck_Pro_Micro.ino
@@ -128,6 +128,8 @@ void loop() {
     
     }
 
+    // Fechar Janela
+
     if (digitalRead(botao_4) == LOW) {  // Se o botão estiver pressionado, então:
   
       delay(150);  // Aguarda um curto período para evitar debounce
@@ -160,6 +162,54 @@ void loop() {
       Keyboard.releaseAll();  // Solta todas as teclas
         
       delay(5000);  // Aguarda um tempo para o prompt de comando aparecer
+      digitalWrite(led, LOW); // Desliga o led
+    
+    }
+    
+    // Menu de Configurações Rapidas
+
+    if (digitalRead(botao_6) == LOW) {  // Se o botão estiver pressionado, então:
+  
+      delay(150);  //Aguarda um curto período para evitar debounce
+      digitalWrite(led, HIGH); // Liga o led
+    
+      Keyboard.press(KEY_LEFT_GUI);  // Pressiona a tecla Windows/Iniciar/Super
+      Keyboard.press('a');  // Pressiona a tecla A
+      Keyboard.releaseAll();  // Solta todas as teclas
+
+      delay(2000);  // Aguarda um tempo para as configurações rapidas aparecer
+      digitalWrite(led, LOW); // Desliga o led
+    
+    }
+
+    // Abrir as Configurações Gerais do PC
+
+    if (digitalRead(botao_7) == LOW) {  // Se o botão estiver pressionado, então:
+  
+      delay(150);  //Aguarda um curto período para evitar debounce
+      digitalWrite(led, HIGH); // Liga o led
+    
+      Keyboard.press(KEY_LEFT_GUI);  // Pressiona a tecla Windows/Iniciar/Super
+      Keyboard.press('i');  // Pressiona a tecla I
+      Keyboard.releaseAll();  // Solta todas as teclas
+
+      delay(2000);  // Aguarda um tempo para as configurações aparecer
+      digitalWrite(led, LOW); // Desliga o led
+    
+    }
+
+    // Abre o Menu Notificações
+
+    if (digitalRead(botao_8) == LOW) {  // Se o botão estiver pressionado, então:
+  
+      delay(150);  //Aguarda um curto período para evitar debounce
+      digitalWrite(led, HIGH); // Liga o led
+    
+      Keyboard.press(KEY_LEFT_GUI);  // Pressiona a tecla Windows/Iniciar/Super
+      Keyboard.press('n');  // Pressiona a tecla N
+      Keyboard.releaseAll();  // Solta todas as teclas
+
+      delay(2000);  // Aguarda um tempo para as notificações aparecerem
       digitalWrite(led, LOW); // Desliga o led
     
     }
